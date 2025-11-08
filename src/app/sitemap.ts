9d54f54952dde0b3e0630bad8f0680ea3805b55f
vedantlavale <vedantlavale@gmail.com>
2025-11-08 11:43:15 +0530

    update url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://mdify.vercel.app'
+  const baseUrl = 'https://medium-to-md-vert.vercel.app'
   
   return [
     {
